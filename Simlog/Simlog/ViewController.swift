e93b7730e5c24e08f45ac35b17fd3ea5c91b8e62
ehvkddl <ehvkddl@naver.com>
2023-09-25 16:08:29 +0900

    init: Simlog 생성

---
+++ Simlog/Simlog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simlog
+//
+//  Created by do hee kim on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
