7e85061fe2854aad52c1421246a69a2b4e9e7f02
ehvkddl <ehvkddl@naver.com>
2023-09-26 04:15:35 +0900

    add: TabBar에 연결할 TempView들 추가 #1

---
+++ Simlog/Simlog/ViewControllers/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  Simlog
+//
+//  Created by do hee kim on 2023/09/26.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+
+}
