b739539bbf086ee08d4155333f0e5b632aea4c40
ehvkddl <ehvkddl@naver.com>
2023-09-27 02:43:27 +0900

    add: ConfigureNavigationBar 메서드 추가, Color 추가 #3

--- Simlog/Simlog/Utility/Color.swift
+++ Simlog/Simlog/Utility/Color.swift
@@ -11,9 +11,12 @@ enum Constants {
     
     enum BaseColor {
         static let accent = UIColor.label
-        static let background = UIColor.systemBackground
+        static let background = UIColor(named: "Background")
+        static let grayBackground = UIColor(named: "GrayBackground")
+        static let containerBackground = UIColor(named: "ContainerBackground")
         static let border = UIColor.label.cgColor
         static let text = UIColor.label
+        static let reverseText = UIColor.systemBackground
         static let placeholder = UIColor.lightGray
     }
     
