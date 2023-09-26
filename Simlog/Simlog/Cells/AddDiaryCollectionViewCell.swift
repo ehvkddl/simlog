cded6ff99efb364b7c01d3b05eb190f429fbcaca
ehvkddl <ehvkddl@naver.com>
2023-09-27 04:06:10 +0900

    feat: AddDiaryView test #5

---
+++ Simlog/Simlog/Cells/AddDiaryCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  AddDiaryCollectionViewCell.swift
+//  Simlog
+//
+//  Created by do hee kim on 2023/09/27.
+//
+
+import UIKit
+
+class AddDiaryCollectionViewCell: BaseCollectionViewCell {
+    
+    let containerView = {
+        let view = UIView()
+        view.backgroundColor = Constants.BaseColor.containerBackground
+        view.layer.cornerRadius = 15
+        return view
+    }()
+    
+    let titleLabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        return lbl
+    }()
+    
+    override func configureCell() {
+        [containerView].forEach { contentView.addSubview($0) }
+        [titleLabel].forEach { containerView.addSubview($0) }
+    }
+    
+    override func setConstraints() {
+        containerView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.leading.equalTo(containerView).inset(15)
+        }
+    }
+}
