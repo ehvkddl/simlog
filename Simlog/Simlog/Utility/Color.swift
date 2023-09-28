f7b7f598cfc3f545e092046059119831f91c6f78
ehvkddl <ehvkddl@naver.com>
2023-09-29 05:30:11 +0900

    add: Tick Color 추가 #5

--- Simlog/Simlog/Utility/Color.swift
+++ Simlog/Simlog/Utility/Color.swift
@@ -11,9 +11,10 @@ enum Constants {
     
     enum BaseColor {
         static let accent = UIColor.label
-        static let background = UIColor(named: "Background")
-        static let grayBackground = UIColor(named: "GrayBackground")
-        static let containerBackground = UIColor(named: "ContainerBackground")
+        static let background = UIColor(named: "Background")!
+        static let grayBackground = UIColor(named: "GrayBackground")!
+        static let containerBackground = UIColor(named: "ContainerBackground")!
+        static let tick = UIColor(named: "Tick")!
         static let border = UIColor.label.cgColor
         static let text = UIColor.label
         static let reverseText = UIColor.systemBackground
