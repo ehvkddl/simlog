d205ba3f1bf1107f500645bf2b1407a0ff76695f
ehvkddl <ehvkddl@naver.com>
2023-09-29 05:36:51 +0900

    feat: DrawTick 시계 모양 #5

--- Simlog/Simlog/ViewControllers/BedTimeViewController.swift
+++ Simlog/Simlog/ViewControllers/BedTimeViewController.swift
@@ -38,25 +38,34 @@ class BedTimeViewController: BaseViewController {
     lazy var rangeCircularSlider = {
         let slider = RangeCircularSlider()
         slider.backgroundColor = .clear
-        slider.startThumbImage = UIImage(systemName: "moon.zzz.fill")?.withTintColor(.white)
-        slider.endThumbImage = UIImage(systemName: "sun.max.fill")?.withTintColor(.white)
-        slider.backtrackLineWidth = 35.0
-        slider.lineWidth = 35.0
-        slider.trackFillColor = .black
-        
+        slider.startThumbImage = UIImage(systemName: "moon.zzz.fill")?.withTintColor(Constants.BaseColor.background)
+        slider.endThumbImage = UIImage(systemName: "sun.max.fill")?.withTintColor(Constants.BaseColor.background)
+        slider.backtrackLineWidth = 40.0
+        slider.lineWidth = 40.0
+        slider.trackColor = Constants.BaseColor.grayBackground
+        slider.trackFillColor = Constants.BaseColor.accent
+        slider.diskColor = .clear
+        slider.diskFillColor = Constants.BaseColor.grayBackground
         return slider
     }()
     
+    let clockImageView = {
+        let view = UIImageView()
+        view.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        drawTicks(count: 60)
     }
     
     override func configureView() {
         closeButton.addTarget(self, action: #selector(closeButtonClicked), for: .touchUpInside)
         
         [backgroundView, contentsView].forEach { view.addSubview($0) }
-        [closeButton, rangeCircularSlider].forEach { contentsView.addSubview($0) }
-        
+        [closeButton, rangeCircularSlider, clockImageView].forEach { contentsView.addSubview($0) }
     }
     
     override func setConstraints() {
@@ -77,7 +86,12 @@ class BedTimeViewController: BaseViewController {
         
         rangeCircularSlider.snp.makeConstraints { make in
             make.center.equalTo(contentsView)
-            make.size.equalTo(300)
+            make.size.equalTo(contentsView.snp.width).multipliedBy(0.95)
+        }
+        
+        clockImageView.snp.makeConstraints { make in
+            make.center.equalTo(contentsView)
+            make.size.equalTo(contentsView.snp.width).multipliedBy(0.57)
         }
     }
     
@@ -87,6 +101,35 @@ extension BedTimeViewController {
     
     @objc private func closeButtonClicked() {
         dismiss(animated: true)
+    }
+    
+}
+
+extension BedTimeViewController {
+    
+    private func drawTicks(count: Int) {
+        let radius = clockImageView.frame.width * 0.5
+        var rotationInDegrees: CGFloat = 0
+
+        for i in 0 ..< count {
+            let tick = i % 5 == 0 ? createTick(height: 10.0) : createTick(height: 5.0)
+
+            let x = CGFloat(Float(clockImageView.center.x) + Float(radius) * cosf(2 * Float(i) * Float(Double.pi) / Float(count) - Float(Double.pi) / 2))
+            let y = CGFloat(Float(clockImageView.center.y) + Float(radius) * sinf(2 * Float(i) * Float(Double.pi) / Float(count) - Float(Double.pi) / 2))
+
+            tick.center = CGPoint(x: x, y: y)
+            tick.transform = CGAffineTransform.identity.rotated(by: rotationInDegrees * .pi / 180.0)
+            clockImageView.addSubview(tick)
+
+            rotationInDegrees = rotationInDegrees + (360.0 / CGFloat(count))
+        }
+    }
+
+    private func createTick(height: Double) -> UIView {
+        let tick = UIView(frame: CGRect(x: 0, y: 0, width: 1.5, height: height))
+        tick.backgroundColor = Constants.BaseColor.tick
+
+        return tick
     }
     
 }
