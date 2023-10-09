db42d2277bc22a604aa8ed613690959cdab95718
ehvkddl <ehvkddl@naver.com>
2023-10-09 22:39:33 +0900

    add: DailyLogViewModel 추가 #5

---
+++ Simlog/Simlog/ViewModels/DailyLogViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  DailyLogViewModel.swift
+//  Simlog
+//
+//  Created by do hee kim on 2023/10/05.
+//
+
+import Foundation
+
+class DailyLogViewModel {
+    
+    var dailylog: Observable<DailyLog> = Observable(DailyLog())
+    
+}
