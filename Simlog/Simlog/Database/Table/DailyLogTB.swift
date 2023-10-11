9bf46ce288b82bd508213599ef3a20b91dda531b
ehvkddl <ehvkddl@naver.com>
2023-10-11 23:54:29 +0900

    add: DailyLog table 생성 #5

---
+++ Simlog/Simlog/Database/Table/DailyLogTB.swift
@@ -0,0 +1,56 @@
+//
+//  TDailyLog.swift
+//  Simlog
+//
+//  Created by do hee kim on 2023/10/11.
+//
+
+import Foundation
+import RealmSwift
+
+class DailyLogTB: Object {
+    @Persisted(primaryKey: true) var _id: String
+    @Persisted var date: Date
+    @Persisted var mood: Int
+    @Persisted var weather: List<Int>
+    @Persisted var sleep: SleepTB?
+    @Persisted var diary: String?
+
+    convenience init(
+        _id: String,
+        date: Date,
+        mood: Int,
+        weather: List<Int>,
+        sleep: SleepTB? = nil,
+        diary: String? = nil
+    ) {
+        self.init()
+        
+        self._id = _id
+        self.date = date
+        self.mood = mood
+        self.weather = weather
+        self.sleep = sleep
+        self.diary = diary
+    }
+}
+
+class SleepTB: Object {
+    @Persisted(primaryKey: true) var _id: String
+    @Persisted var bedTime: Float
+    @Persisted var wakeupTime: Float
+    
+    @Persisted(originProperty: "sleep") var dailyLog: LinkingObjects<DailyLogTB>
+    
+    convenience init(
+        _id: String,
+        bedTime: Float,
+        wakeupTime: Float
+    ) {
+        self.init()
+        
+        self._id = _id
+        self.bedTime = bedTime
+        self.wakeupTime = wakeupTime
+    }
+}
